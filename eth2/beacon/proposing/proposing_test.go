c4d080aa0ba45591e90f4a27abff09d2618edb73
keks <keks@cryptoscope.co>
2020-02-07 13:04:38 +0100

    report version

--- eth2/beacon/proposing/proposing_test.go
+++ eth2/beacon/proposing/proposing_test.go
@@ -8,6 +8,8 @@ import (
 
 	. "github.com/protolambda/zrnt/eth2/core"
 	"github.com/protolambda/zrnt/eth2/meta"
+
+	"github.com/montanaflynn/stats"
 )
 
 func eth2gwei(eths []uint64) []Gwei {
@@ -32,121 +34,193 @@ const (
 
 func TestProposerDistribution(t *testing.T) {
 	const (
-		times = 48
-		N     = Mi
+		times = 32
+		N     = 64 * Ki
 	)
 
-	var (
-		tcs = []distTestCase{
-			{
-				Name:           "blll",
-				N:              N,
-				BigBalance:     32 * Gi,
-				SmallBalance:   16 * Gi,
-				ValidatorCount: 4,
-			},
-			{
-				Name:           "lbll",
-				N:              N,
-				BigBalance:     32 * Gi,
-				SmallBalance:   16 * Gi,
-				ValidatorCount: 4,
-				BigOffset:      1,
-			},
-			{
-				Name:           "blll..8",
-				N:              N,
-				BigBalance:     32 * Gi,
-				SmallBalance:   16 * Gi,
-				ValidatorCount: 8,
-			},
-			{
-				Name:           "lbll..8",
-				N:              N,
-				BigBalance:     32 * Gi,
-				SmallBalance:   16 * Gi,
-				ValidatorCount: 8,
-				BigOffset:      1,
-			},
-			{
-				Name:           "blll..16",
-				N:              N,
-				BigBalance:     32 * Gi,
-				SmallBalance:   16 * Gi,
-				ValidatorCount: 16,
-			},
-			{
-				Name:           "lbll..16",
-				N:              N,
-				BigBalance:     32 * Gi,
-				SmallBalance:   16 * Gi,
-				ValidatorCount: 16,
-				BigOffset:      1,
-			},
-			{
-				Name:           "blll..32",
-				N:              N,
-				BigBalance:     32 * Gi,
-				SmallBalance:   16 * Gi,
-				ValidatorCount: 32,
-			},
-			{
-				Name:           "lbll..32",
-				N:              N,
-				BigBalance:     32 * Gi,
-				SmallBalance:   16 * Gi,
-				ValidatorCount: 32,
-				BigOffset:      1,
-			},
-			{
-				Name:           "blll..64",
-				N:              N,
-				BigBalance:     32 * Gi,
-				SmallBalance:   16 * Gi,
-				ValidatorCount: 64,
-			},
-			{
-				Name:           "lbll..64",
-				N:              N,
-				BigBalance:     32 * Gi,
-				SmallBalance:   16 * Gi,
-				ValidatorCount: 64,
-				BigOffset:      1,
-			},
-		}
-		results = make(chan distTestResult, times*len(tcs))
-	)
+	var tcs []distTestCase
 
-	f, err := os.Create("results")
+	// small set sizes
+	for i := uint(3); i < 6; i++ {
+		validatorCount := 1 << i
+		tcs = append(tcs,
+			distTestCase{
+				Name:           fmt.Sprintf("%d-0-%d", validatorCount, N),
+				N:              N,
+				BigBalance:     32 * Gi,
+				SmallBalance:   16 * Gi,
+				ValidatorCount: validatorCount,
+				BigOffset:      0,
+			},
+			distTestCase{
+				Name:           fmt.Sprintf("%d-1-%d", validatorCount, N),
+				N:              N,
+				BigBalance:     32 * Gi,
+				SmallBalance:   16 * Gi,
+				ValidatorCount: validatorCount,
+				BigOffset:      1,
+			})
+	}
+
+	// set of 128 validators with different offsets of large validator
+	for i := 0; i < 2; i++ {
+		tcs = append(tcs, distTestCase{
+			Name:           fmt.Sprintf("4096-%d-%d", i, N),
+			N:              N,
+			BigBalance:     32 * Gi,
+			SmallBalance:   16*Gi + 1*Ki,
+			ValidatorCount: 128,
+			BigOffset:      i,
+		})
+	}
+
+	// larger validator sets with variable number of elections
+	for i := uint(3); i < 13; i++ {
+		validatorCount := 1 << i
+		electionCount := 1 << (i + 10)
+		tcs = append(tcs,
+			distTestCase{
+				Name:           fmt.Sprintf("%d-0-%d", validatorCount, electionCount),
+				N:              electionCount,
+				BigBalance:     32 * Gi,
+				SmallBalance:   16 * Gi,
+				ValidatorCount: validatorCount,
+				BigOffset:      0,
+			},
+			distTestCase{
+				Name:           fmt.Sprintf("%d-1-%d", validatorCount, electionCount),
+				N:              electionCount,
+				BigBalance:     32 * Gi,
+				SmallBalance:   16 * Gi,
+				ValidatorCount: validatorCount,
+				BigOffset:      1,
+			},
+		)
+	}
+
+	// 4Ki validator set with different number of elections
+	for i := uint(3); i < 9; i++ {
+		electionCount := 4096 << i
+		tcs = append(tcs,
+			distTestCase{
+				Name:           fmt.Sprintf("4096-0-%d", electionCount),
+				N:              electionCount,
+				BigBalance:     32 * Gi,
+				SmallBalance:   16 * Gi,
+				ValidatorCount: 4096,
+				BigOffset:      0,
+			},
+			distTestCase{
+				Name:           fmt.Sprintf("4096-1-%d", electionCount),
+				N:              electionCount,
+				BigBalance:     32 * Gi,
+				SmallBalance:   16 * Gi,
+				ValidatorCount: 4096,
+				BigOffset:      1,
+			},
+		)
+	}
+
+	// 16Ki validators with 128Ki elections
+	func() {
+		const (
+			nValidators = 16 * Ki
+			nElections  = nValidators << 3
+		)
+
+		tcs = append(tcs,
+			distTestCase{
+				Name:           fmt.Sprintf("%d-0-%d", nValidators, nElections),
+				N:              nElections,
+				BigBalance:     32 * Gi,
+				SmallBalance:   16 * Gi,
+				ValidatorCount: nValidators,
+				BigOffset:      0,
+			},
+			distTestCase{
+				Name:           fmt.Sprintf("%d-1-%d", nValidators, nElections),
+				N:              nElections,
+				BigBalance:     32 * Gi,
+				SmallBalance:   16 * Gi,
+				ValidatorCount: nValidators,
+				BigOffset:      1,
+			},
+		)
+	}()
+
+	var results = make(chan distTestResult, times*len(tcs))
+
+	fmt.Fprintln(os.Stderr, "writing test data to results.csv")
+
+	f, err := os.OpenFile("results.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	// write table headers if file was opened for the first time
+	if fi, err := f.Stat(); err == nil && fi.Size() == 0 {
+		// what a mouthful
+		_, err = fmt.Fprintln(f, "validatorCount,electionsCount,bigValidatorIndex,"+
+			"bigValidatorElectionsAvg,bigValidatorElectionsStdev,bigValidatorElectionsRelStdev,"+
+			"smallValidatorElectionsAvg,smallValidatorElectionsStdev,smallValidatorElectionsRelStdev,"+
+			"ratioBigAvgToSmallAvg")
+	}
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	t.Run("manage", func(t *testing.T) {
-		t.Parallel()
-		defer f.Close()
+	go func() {
+		type average struct {
+			BigCount      float32
+			SmallCountAvg float32
+		}
 
 		var (
-			ratioAvgs  = make(map[string]float32)
-			doneMap    = make(map[string]int)
-			res        distTestResult
-			resultList []distTestResult
+			avg            average
+			avgs           = make(map[string]average)
+			bigCounts      = make(map[string][]float64)
+			smallCountAvgs = make(map[string][]float64)
+			doneMap        = make(map[string]int)
+			res            distTestResult
 		)
 
 		for i := 0; i < times*len(tcs); i++ {
 			res = <-results
 
-			ratioAvgs[res.Name] += float32(res.BigCount()) / float32(res.SmallCountAvg()) / times
+			avg = avgs[res.Name]
+			avg.BigCount += float32(res.BigCount()) / times
+			avg.SmallCountAvg += float32(res.SmallCountAvg()) / times
+			avgs[res.Name] = avg
+
+			bigCounts[res.Name] = append(bigCounts[res.Name], float64(res.BigCount()))
+			smallCountAvgs[res.Name] = append(smallCountAvgs[res.Name], float64(res.SmallCountAvg()))
+
 			doneMap[res.Name] += 1
-			resultList = append(resultList, res)
 
 			if doneMap[res.Name] == times {
-				fmt.Fprintf(f, "%s,%d,%d,%f\n", res.Name, res.BigCount(), res.SmallCountAvg(), ratioAvgs[res.Name])
+				bigStdev, err := stats.StandardDeviation(bigCounts[res.Name])
+				if err != nil {
+					panic(err)
+				}
+
+				smallStdev, err := stats.StandardDeviation(smallCountAvgs[res.Name])
+				if err != nil {
+					panic(err)
+				}
+
+				fmt.Fprintf(f,
+					"%d,%d,%d,"+
+						"%f,%f,%f,"+
+						"%f,%f,%f,"+
+						"%f\n",
+					res.ValidatorCount, res.N, res.BigOffset,
+					avg.BigCount, bigStdev, float32(bigStdev)/avg.BigCount,
+					avg.SmallCountAvg, smallStdev, float32(smallStdev)/avg.SmallCountAvg,
+					avg.BigCount/avg.SmallCountAvg)
 			}
 		}
-
-		t.Log(ratioAvgs)
-	})
+	}()
 
 	for _, tc := range tcs {
 
@@ -171,8 +245,8 @@ func (tr distTestResult) BigCount() int {
 	return tr.Counts[tr.BigOffset]
 }
 
-func (tr distTestResult) SmallCountAvg() int {
-	return (sum(tr.Counts) - tr.Counts[tr.BigOffset]) / (tr.ValidatorCount - 1)
+func (tr distTestResult) SmallCountAvg() float32 {
+	return float32(sum(tr.Counts)-tr.Counts[tr.BigOffset]) / float32(tr.ValidatorCount-1)
 }
 
 type distTestCase struct {
@@ -205,15 +279,19 @@ func (tc distTestCase) makeTest(round int, results chan<- distTestResult) func(*
 		balances[tc.BigOffset] = tc.BigBalance
 
 		// prepare logging format strings
-		l := len(fmt.Sprintf("%d", tc.N))
-		formatN := fmt.Sprintf("N=%%%dd\n", l)
-		formatI := fmt.Sprintf("i=%%%dd\n", l)
+		/*
+			l := len(fmt.Sprintf("%d", tc.N))
+			formatN := fmt.Sprintf("N=%%%dd\n", l)
+			formatI := fmt.Sprintf("i=%%%dd\n", l)
 
-		fmt.Printf(formatN, tc.N)
+			fmt.Printf(formatN, tc.N)
+		*/
 		for i := 0; i < tc.N; i++ {
-			if i&0xffff == 0 {
-				fmt.Printf(formatI, i)
-			}
+			/*
+				if i&0xffff == 0 {
+					fmt.Printf(formatI, i)
+				}
+			*/
 
 			var seed Root
 			rand.Read(seed[:])
@@ -228,17 +306,19 @@ func (tc distTestCase) makeTest(round int, results chan<- distTestResult) func(*
 			Counts:       counts,
 		}
 
-		var (
-			smallAvg      = (sum(counts) - counts[tc.BigOffset]) / (len(balances) - 1)
-			countFactor   = float32(counts[tc.BigOffset]) / float32(smallAvg)
-			depositFactor = float32(balances[1]) / float32(balances[0])
-		)
+		/*
+			var (
+				smallAvg      = (sum(counts) - counts[tc.BigOffset]) / (len(balances) - 1)
+				countFactor   = float32(counts[tc.BigOffset]) / float32(smallAvg)
+				depositFactor = float32(balances[1]) / float32(balances[0])
+			)
 
-		t.Log("count of big validator:", counts[tc.BigOffset])
-		t.Log("average of small validators:", smallAvg)
-		t.Log("large validator count is ahead by factor:", countFactor)
-		t.Log("large validator deposit is ahead by factor:", depositFactor)
-		t.Log("averate iterations between election of single little validator:", tc.N/smallAvg)
+			t.Log("count of big validator:", counts[tc.BigOffset])
+			t.Log("average of small validators:", smallAvg)
+			t.Log("large validator count is ahead by factor:", countFactor)
+			t.Log("large validator deposit is ahead by factor:", depositFactor)
+			t.Log("averate iterations between election of single little validator:", tc.N/smallAvg)
+		*/
 	}
 }
 
